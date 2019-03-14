b9a0298f031977c9a3c8e676f7ae63398a759b2f
Patrick <patrick.schwindt14@gmail.com>
2019-03-14 16:45:59 -0600

    Commit

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          <li>
+            <Link to='/home'>Home</Link>
+          </li>
+          <li>
+            <Link to='/home-protected'>Protected-Home</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/home-protected" component={''} />
+
       </div>
+      </Router>
     );
   }
 }
