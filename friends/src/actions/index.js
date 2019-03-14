717eb2fb5135d8e4e545285f1e9bf9c0395be617
Patrick <patrick.schwindt14@gmail.com>
2019-03-14 16:46:41 -0600

    Commit

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -5,7 +5,7 @@ export const LOGIN_START = "LOGIN_START";
 
 export const login = creds => dispatch =>{
     dispatch({type: LOGIN_START});
-    return axios.post('http://localhost:5000/api/login', creds).then(res =>{
+    return axios.post('http://localhost:5000/login', creds).then(res =>{
         localStorage.setItem('token', res.data.payload)
     })
 }
